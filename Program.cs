641734a6ca06bc3d5187bf821c8101ad3d9536d0
Даниил Полянский <dm.tsiganov@gmail.com>
2022-11-23 14:13:13 +0700

    23

--- Program.cs
+++ Program.cs
@@ -4,7 +4,8 @@ int cube = Convert.ToInt32(Console.ReadLine());
 void Cube(int[] cube){
   int counter = 0;
   int length = cube.Length;
-  while (counter <  length){
+  while (counter <  length)
+  {
     cube[counter] = Convert.ToInt32(Math.Pow(counter, 3));
     counter++;
   }
