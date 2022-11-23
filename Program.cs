cc3e34c38cae9500b18dbdd4864f3f977b58a087
Даниил Полянский <dm.tsiganov@gmail.com>
2022-11-23 13:56:34 +0700

    19

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿Console.WriteLine("Введите число "); 
+int Number = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine(P(Number));        
+Console.ReadKey(true);        
+
+static bool P(int Number)      
+{
+    if (Number >= 0 && Number <10)        
+    return true;
+    int NumberLength = GetLength(Number);
+    int[] digits = new int[NumberLength];
+        for (int i = NumberLength - 1; i >= 0; i--) 
+        {
+            digits[i] = Number % 10;
+            Number /= 10;
+        }
+             for (int i = 0; i < NumberLength/2; i++)
+             {
+                if (digits[i] != digits[NumberLength - i - 1])
+                return false;
+             }
+            return true;            
+}        
+               
+static int GetLength(int Number)        
+{
+    int N = 0;
+    while (Number > 0)
+    {
+        Number /= 10;
+        N++;
+    }
+    return N;
+ }
